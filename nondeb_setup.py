4430d3a75aed990ce72db6ad13568880e742f304
Fernand Veilleux <fernveilleux@gmail.com>
2017-03-16 01:46:59 -0400

    convert py files to Unix format
    Signed-off-by: Fernand Veilleux <fernveilleux@gmail.com>

--- nondeb_setup.py
+++ nondeb_setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 '''
 Will create links and modify files to embed NativeCAM in LinuxCNC
--- tr_glade.py
+++ tr_glade.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# coding: utf-8
 '''
 Created on 2017-03-13
 
